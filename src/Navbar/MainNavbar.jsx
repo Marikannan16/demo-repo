4c2cf95d592e7c6ec566a60f509b78e573c24274
DeepikaRamalakshmi <deepika129r@gmail.com>
2024-11-18 17:30:40 +0530

    My Initial commit

--- src/Navbar/MainNavbar.jsx
+++ src/Navbar/MainNavbar.jsx
@@ -3,7 +3,7 @@ import { FaBars, FaRegBell, FaRegUserCircle } from "react-icons/fa";
 
 const MainNavbar = ({ toggleSidebar }) => {
     return (
-        <div className="bg-white shadow-lg text-white py-2 px-3 flex items-center justify-between">
+        <div className="bg-blue-600 shadow-lg text-white py-2 px-3 flex items-center justify-between">
             <FaBars onClick={toggleSidebar} className='cursor-pointer text-2xl rounded bg-primary p-1 ' />
 
             <div className="flex gap-2">
